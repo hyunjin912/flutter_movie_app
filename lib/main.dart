7f055d3942ee1feea0a79f371e681982864bd07b
leehyunjin <skyhyunjinlee@gmail.com>
2025-05-13 00:07:16 +0900

    chore : riverpod 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_movie_app/presentation/pages/detail/detail.dart';
 import 'package:flutter_movie_app/presentation/pages/home/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
