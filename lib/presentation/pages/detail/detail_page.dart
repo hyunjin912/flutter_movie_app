072ba979ae502439f4cbaa23f1d9d0b14291aa0e
leehyunjin <skyhyunjinlee@gmail.com>
2025-05-14 14:17:44 +0900

    docs : 안쓰는 import 제거

--- lib/presentation/pages/detail/detail_page.dart
+++ lib/presentation/pages/detail/detail_page.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_app/domain/entity/movie_detail.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/box_office_info_list.dart';
 import 'package:flutter_movie_app/presentation/pages/detail/widgets/detail_scaffold.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/genre_list.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/movie_poster.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/movie_title.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/overview.dart';
-import 'package:flutter_movie_app/presentation/pages/detail/widgets/production_company_logo_list.dart';
 import 'package:flutter_movie_app/presentation/pages/widgets/global_scaffold.dart';
-import 'package:intl/intl.dart';
 
 class DetailPage extends StatelessWidget {
   final MovieDetail? movie;
