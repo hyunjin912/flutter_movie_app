10c8f593b857f39a354fa57f03331e8eefabe5d0
leehyunjin <skyhyunjinlee@gmail.com>
2025-05-14 10:55:26 +0900

    docs : 주석 추가

--- lib/core/font.dart
+++ lib/core/font.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// 공통된 폰트 스타일 정의
 class Font {
   static final style = TextStyle(color: Colors.grey);
 }
--- lib/data/data_source/movie_data_source.dart
+++ lib/data/data_source/movie_data_source.dart
@@ -2,9 +2,18 @@ import 'package:flutter_movie_app/data/dto/movie_detail_dto.dart';
 import 'package:flutter_movie_app/data/dto/movie_response_dto.dart';
 
 abstract interface class MovieDataSource {
+  /// 인기 영화 목록 가져오기
   Future<List<MovieResponseDto>> fetchPopularMovies();
+
+  /// 현재 상영중인 영화 목록 가져오기
   Future<List<MovieResponseDto>> fetchNowPlayingMovies();
+
+  /// 평점 높은 순으로 영화 목록 가져오기
   Future<List<MovieResponseDto>> fetchTopRatedMovies();
+
+  /// 개봉 예정인 영화 목록 가져오기
   Future<List<MovieResponseDto>> fetchUpcomingMovies();
+
+  /// 해당 영화의 자세한 정보 가져오기
   Future<MovieDetailDto?> fetchMovieDetail(int id);
 }
